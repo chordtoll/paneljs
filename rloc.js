57a432fee1ffbdc71f68c3f146abb960b5f65aa8
Andrew Sentman <asentman.s6@gmail.com>
2017-09-25 22:33:00 -0700

    Separated rendering logic from HTML page

--- rloc.js
+++ rloc.js
@@ -1,4 +1,4 @@
-var EMPTY='----'
+var EMPTY='    '
 var rloc=[
 ['B0'  ,'F0'  ,'1`'  ,'2`'  ,'3`'  ,'4`'  ,'5`'  ,'6`'  ,'7`'  ,'8`'  ,'9`'  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,'L1'  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,'TN'  ],
 [EMPTY ,'0'   ,'1'   ,'2'   ,'3'   ,'4'   ,'5'   ,'6'   ,'7'   ,'8'   ,'9'   ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,'TG'  ,'MTG' ],
